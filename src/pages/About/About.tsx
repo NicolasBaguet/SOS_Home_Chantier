1281575c86b4019a3ab0bcd91888b0f9ba41e1e5
Nico <nyco.baguet@gmail.com>
2025-03-18 18:04:05 +0100

    contenu home/about + ajustement h3 en mobile

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -17,6 +17,133 @@ function About() {
 				</h2>
 				<h2 className="citationAuthor">Charlotte Moss</h2>
 			</div>
+			<section className="content">
+				<h3>Mes Valeurs</h3>
+				<div className="container">
+					<div className="serviceA">
+						<div className="serviceImg">
+							<img src="/plan_2.jpg" alt="dessin ou logo" />
+						</div>
+						<div className="serviceTxt">
+							<h4>Respect de notre environnement</h4>
+							<p>
+								Je suis convaincu(e) que le design d'intérieur peut sublimer vos
+								espaces tout en respectant notre planète. C'est pourquoi chaque
+								projet que je réalise s'inscrit dans une démarche
+								écoresponsable.
+							</p>
+							<p>
+								Je privilégie des matériaux durables, recyclés ou recyclables,
+								qui allient esthétique et respect de l'environnement. Chaque
+								choix est pensé pour créer un équilibre parfait entre élégance
+								et durabilité.
+							</p>
+							<p>
+								J'adore redonner vie aux meubles et objets existants. En les
+								transformant avec soin, ils deviennent des pièces uniques qui
+								racontent une histoire tout en réduisant les déchets. C'est
+								aussi une belle façon d'apporter une touche personnelle et
+								authentique à votre intérieur.
+							</p>
+							<p>
+								Pour aller plus loin, je mets un point d'honneur à limiter
+								l'empreinte écologique de mes chantiers. Cela passe par un tri
+								rigoureux des déchets et leur recyclage systématique, afin de
+								préserver au mieux notre environnement.
+							</p>
+						</div>
+					</div>
+					<div className="serviceB">
+						<div className="serviceTxt">
+							<h4>L'Amour du Métier</h4>
+							<p>
+								Un amour profond pour le design se traduit par une attention
+								minutieuse portée à chaque détail. J’accorde une importance
+								particulière à comprendre pleinement les besoins de mes clients,
+								tout en m’engageant dans une quête constante d’innovation et de
+								créativité.
+							</p>
+							<p>
+								Mon enthousiasme pour l’optimisation des espaces me pousse à
+								rester à la pointe des tendances et des technologies, afin de
+								livrer des réalisations uniques qui dépassent les attentes.
+							</p>
+							<p>
+								Chaque projet devient pour moi une occasion précieuse de
+								concevoir un espace sur mesure, à la fois esthétique et
+								fonctionnel.{" "}
+							</p>
+							<p>
+								Cette passion transparaît dans la satisfaction de mes clients et
+								dans la pérennité des intérieurs que je crée, transformant
+								chaque lieu en une véritable œuvre d’art vivante.
+							</p>
+						</div>
+						<div className="serviceImg">
+							<img src="/travail_1.jpg" alt="dessin ou logo" />
+						</div>
+					</div>
+					<div className="serviceA">
+						<div className="serviceImg">
+							<img src="/travail_2.jpg" alt="dessin ou logo" />
+						</div>
+						<div className="serviceTxt">
+							<h4>Écoute des Besoins</h4>
+							<p>
+								Chaque espace à une histoire unique. C'est pourquoi notre
+								processus commence toujours par une écoute attentive de vos
+								aspirations. Dès notre première rencontre, je plonge dans
+								l'univers de vos désirs, mon objectif est de saisir l'essence de
+								votre personnalité, votre style de vie et vos ambitions pour
+								votre espace La communication est primordiale pour moi.
+							</p>
+							<p>
+								 Tout au long de votre projet, je maintiens un échange constant
+								via des appels téléphoniques, des échanges par e-mail et des
+								réunions en personne.
+							</p>
+							<p>
+								Cette approche collaborative garantit que chaque décision
+								reflète vos souhaits et que le résultat final dépasse vos
+								attentes.
+							</p>
+						</div>
+					</div>
+					<div className="serviceB">
+						<div className="serviceTxt">
+							<h4>L'adaptabilité et l'ingéniosité</h4>
+							<p>
+								L'adaptabilité et l'ingéniosité sont deux qualités essentielles
+								qui forment le socle de l'expertise d'un architecte d'intérieur.
+								Ces compétences complémentaires permettent au professionnel de
+								relever les défis uniques de chaque projet avec brio. Face à des
+								contraintes spatiales, budgétaires ou esthétiques, l'architecte
+								d'intérieur puise dans sa capacité d'adaptation pour proposer
+								des solutions sur mesure, tout en faisant preuve d'ingéniosité
+								pour transformer les obstacles en opportunités créatives.
+							</p>
+							<p>
+								Qu'il s'agisse de repenser l'agencement d'un petit espace pour
+								le rendre plus fonctionnel, ou d'intégrer harmonieusement des
+								éléments modernes dans un bâtiment historique, ces aptitudes
+								combinées lui permettent de créer des intérieurs à la fois
+								pratiques, esthétiques et innovants, répondant parfaitement aux
+								besoins et aux désirs de ses clients.Le secteur de
+								l'architecture d'intérieur évolue rapidement en termes de goûts,
+								de styles et de technologies.
+							</p>
+							<p>
+								L'adaptabilité permet aux professionnels de rester compétitifs
+								et pertinents en s'ajustant continuellement aux nouvelles
+								tendances et aux avancées technologiques.
+							</p>
+						</div>
+						<div className="serviceImg">
+							<img src="/bureau.jpg" alt="dessin ou logo" />
+						</div>
+					</div>
+				</div>
+			</section>
 		</div>
 	);
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -19,6 +19,105 @@ function Home() {
 				<h2 className="citationAuthor">Le Corbusier</h2>
 			</div>
 			<section className="content">
+				<h3>Mon objectif</h3>
+				<div className="container">
+					<div className="service">
+						<div className="serviceImg">
+							<img src="/plan_2.jpg" alt="dessin ou logo" />
+						</div>
+						<div className="serviceTxt">
+							<p>
+								Mon objectif, que vous vous sentiez bien dans un nouveau chez
+								vous sans bousculer vos repères.
+							</p>
+							<p>
+								D’imaginer ensemble, selon vos besoins, des espaces fonctionnels
+								et esthétiques où l'énergie circule librement, pour créer des
+								intérieurs qui reflètent votre personnalité et votre style de
+								vie, où chaque objet trouve sa place et chaque place à son objet
+								dans un équilibre parfait.
+							</p>
+							<p>
+								Chaque détail compte pour concevoir un intérieur en harmonie
+								avec vos aspirations profondes. Des aménagements pensés pour
+								nourrir votre esprit et apaiser votre cœur.
+							</p>
+						</div>
+					</div>
+				</div>
+				<h3>Mes convictions</h3>
+				<div className="container">
+					<div className="service">
+						<div className="serviceImg">
+							<img src="/plan_2.jpg" alt="dessin ou logo" />
+						</div>
+						<div className="serviceTxt">
+							<ul>
+								<li>Penser votre projet</li>
+								<li>
+									C’est apprendre à vous connaître grâce à un dialogue ouvert et
+									franc
+								</li>
+								<li>C’est m’imprégner du lieu pour en déceler le potentiel</li>
+								<li>C’est prendre le temps qu’il faut pour ne pas en perdre</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<h3>Créer votre projet</h3>
+				<div className="container">
+					<div className="service">
+						<div className="serviceImg">
+							<img src="/plan_2.jpg" alt="dessin ou logo" />
+						</div>
+						<div className="serviceTxt">
+							<ul>
+								<li>
+									C’est imaginer une nouvelle histoire qui fait sens pour vous
+									et votre lieu avec audace et originalité grâce à des solutions
+									sur-mesure et innovantes
+								</li>
+								<li>C’est vous offrir un interlocuteur unique et agile</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<h3>Vous accompagner</h3>
+				<div className="container">
+					<div className="service">
+						<div className="serviceImg">
+							<img src="/plan_2.jpg" alt="dessin ou logo" />
+						</div>
+						<div className="serviceTxt">
+							<ul>
+								<li>
+									C’est vous mettre au cœur du processus en vous expliquant ma
+									démarche
+								</li>
+								<li>
+									C’est faire évoluer vos espaces quand vos besoins changeront
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<h3>Mon engagement, votre satisfaction</h3>
+				<div className="container">
+					<div className="service">
+						<div className="serviceImg">
+							<img src="/plan_2.jpg" alt="dessin ou logo" />
+						</div>
+						<div className="serviceTxt">
+							<p>
+								Pour moi, un projet n'est réussi que lorsqu'il incarne
+								parfaitement vos désirs. Je mets donc tout mon savoir-faire et
+								ma passion au service de votre vision, pour créer un espace qui
+								vous ressemble et dans lequel vous vous sentirez pleinement chez
+								vous.
+							</p>
+						</div>
+					</div>
+				</div>
 				<h3>Nos avis</h3>
 				<div className="container">
 					{ratesMessage.map((message) => (
