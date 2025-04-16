6c66282de2a7b68b7da717b9f6a142df397821fb
nico <nyco.baguet@gmail.com>
2025-04-16 20:48:03 +0200

    height header, picture allaire and lions

--- src/pages/Realisations/Realisations.tsx
+++ src/pages/Realisations/Realisations.tsx
@@ -1,6 +1,9 @@
 import "./Realisations.scss";
 import Carousel from "../../components/Carousel/Carousel";
 import photosLions from "../../../data/photosLions.json" with { type: "json" };
+import photosAllaire from "../../../data/photosAllaire.json" with {
+	type: "json",
+};
 import photosDore from "../../../data/photosMontDore.json" with {
 	type: "json",
 };
@@ -52,14 +55,38 @@ function Realisations() {
 							</div>
 						</div>
 						<div className="serviceRate">
-							<h4>Jean-Louis D.</h4>
+							<h4>Jean-Pierre B.</h4>
 							<p>
-								Nous te remercions pour le pré-projet que tu as réalisé en vue
-								de la transformation de la cuisine et la salle de bain. Il nous
-								a permis d’élaborer de façon détaillé avec le constructeur
-								l’aménagement et l'équipement des deux pièces. Le résultat qui a
-								donné lieu à une labellisation "Meublé de Tourisme 3 étoiles"
-								est apprécié par les locataires qui n’hésitent pas à revenir.
+								Nous avons fait appel à Stéphanie BAGUET pour la rénovation de
+								l’agencement professionnel du Pub THE LIONS.
+							</p>
+							<p>
+								{" "}
+								Les travaux ont consisté, dans un premier temps, à mettre à nu
+								le local par la dépose totale de l’agencement en place.{" "}
+							</p>
+							<p>
+								Le travail de Stéphanie a consisté à créer des espaces et
+								aménager ceux-ci dans un style de Pub Anglais. Les espaces ont
+								été aménagés avec plusieurs style de décoration – c’est ainsi
+								qu’il a été réalisé des espaces avec des matériaux différents –
+								murs en briquettes, papiers peints murs et plafonds, menuiseries
+								(cimaises, moulures, espace cheminés) pour la partie clientèle
+								et création d’un bar pour la partie professionnelle.{" "}
+							</p>
+							<p>
+								Les sols ont été parquetés. La partie luminaire n’a pas été
+								oublié puisqu’il a été réalisé un ensemble harmonieux
+								d’éclairage feutré créant une atmosphère conviviale. Avec
+								Stéphanie, nous avons donc réussi à recréer l’atmosphère d’un
+								Pub Anglais dans le pur style Anglo-Irlandais.{" "}
+							</p>
+							<p>
+								L’ensemble est tout à fait réussi. Stéphanie a su être à
+								l’écoute et a compris notre demande – plusieurs projets de
+								décoration et de style ont d’abord été proposés nous laissant le
+								choix du style et de l’atmosphère que nous souhaitions créer.
+								Merci
 							</p>
 						</div>
 					</div>
@@ -99,7 +126,7 @@ function Realisations() {
 					<div className="service">
 						<div className="serviceA">
 							<div className="serviceImg">
-								<Carousel images={photosLions} />
+								<Carousel images={photosAllaire} />
 							</div>
 							<div className="serviceTxt">
 								<h4>3-TROISIEME RENCONTRE</h4>
